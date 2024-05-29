4e51867a7ea5353512d5c2a5f104f1a7cd32ef3a
emddeveloper <emd.developer@gmail.com>
2024-05-29 20:14:54 +0530

    page added

--- src/app/modules/shared/components/payment/payment.component.ts
+++ src/app/modules/shared/components/payment/payment.component.ts
@@ -8,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrl: './payment.component.scss'
 })
 export class PaymentComponent {
-
+  email1="astralstarseed@gmail.com";
+  email2="sanjukta.tarot.mail@gmail.com";
+  mobile1="+918420347137";
+  mobile2="+918420347137";
+  whatsapp1="+918420347137";
 }

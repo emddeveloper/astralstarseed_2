004ef16ca3527e2917c78b1b436d21d6c2637545
emddeveloper <emd.developer@gmail.com>
2024-05-30 19:08:43 +0530

    commit email issue fixed

--- src/app/modules/shared/components/header/header.component.ts
+++ src/app/modules/shared/components/header/header.component.ts
@@ -1,12 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink, RouterModule } from '@angular/router';
+import { SharedService } from '../../services/shared.service';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink,RouterModule],
+  imports: [RouterLink,RouterModule,CommonModule,ReactiveFormsModule,FormsModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
   email1="astralstarseed@gmail.com";
@@ -14,9 +17,43 @@ export class HeaderComponent {
   mobile1="+918420347137";
   mobile2="+918420347137";
   whatsapp1="+918420347137";
-  sessionResponse={
+  sessionResponse : any ={
     mailsent:false,
     error:false,
     msg:"",  
   }
+
+  myForm: FormGroup;
+  isSubmitted = false;
+  loader: boolean;
+  constructor(private fb: FormBuilder, private sharedService : SharedService) {
+    this.myForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      phone: ['', [Validators.required]]
+    });
+  }
+
+  booksession() {
+    if (this.myForm.valid) {
+      this.sharedService.bookSession(this.myForm.value.email,this.myForm.value.phone).subscribe({
+        next : (response ) => {
+          this.sessionResponse = response;
+          console.log(response);
+          this.loader = false; // Hide loader after successful response
+        },
+        error : (error) => {
+          this.sessionResponse = error;
+          console.log(error);
+          this.loader = false; // Hide loader after error
+        },
+        complete: () => {
+          // Handle completion cases
+          this.loader = false; // Hide loader after error
+        }
+      });;
+      console.log('Form submitted!', this.myForm.value);
+      this.isSubmitted = true;
+    }
+  }
+
 }
--- src/app/modules/shared/services/shared.service.ts
+++ src/app/modules/shared/services/shared.service.ts
@@ -1,9 +1,38 @@
+
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
+export interface Iparams {
+  "name":String,
+  "phone":String,
+  "subject":String,
+  "message":String,
+  "email":String
+}
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SharedService {
-
-  constructor() { }
+  public params : Iparams = {
+    "name":'',
+    "phone":'',
+    "subject":'Astralstarseed : Requested for Session Booking',
+    "message":'Please contact me ',
+    "email":''
+  }
+  loader: boolean;
+  sessionResponse: Object;
+  constructor(private http: HttpClient) {}
+  
+  bookSession( email: any,phone: any) {
+    this.loader = true;
+    const sessionBookingParams : Iparams ={
+      "name":'',
+      "phone":phone,
+      "subject":'Astralstarseed : Requested for Session Booking',
+      "message":'Please contact me ',
+      "email":email
+    }
+   return this.http.post('./astralstartseed_blackbox/contactform.php', sessionBookingParams);
+  }
 }

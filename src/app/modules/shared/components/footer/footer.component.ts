4ea8af9d7e73cacbb76de82854a5da48e20d558b
emddeveloper <emd.developer@gmail.com>
2024-06-14 17:53:19 +0530

    14062024 commit done suggestion logo color change, icon color chnge, footer issue link done, top added, rating 5 star testimonial logo removed, unwanted footer link remove,tarot price updated

--- src/app/modules/shared/components/footer/footer.component.ts
+++ src/app/modules/shared/components/footer/footer.component.ts
@@ -1,16 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SharedService } from '../../services/shared.service';
+import { CommonModule } from '@angular/common';
+import { RouterLink, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterModule, CommonModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
-export class FooterComponent {
-  public contactDetails : any;
- constructor(private sharedService : SharedService){
-  this.contactDetails = this.sharedService.getContactDetails();
- }
+export class FooterComponent implements OnInit {
+  public contactDetails: any;
+  public currentYear: number;
+  constructor(private sharedService: SharedService) {
+    this.contactDetails = this.sharedService.getContactDetails();
+  }
+  ngOnInit(): void {
+    this.updateYear();
+  }
+  top(): void {
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: 'smooth'
+    });
+  }
+
+  updateYear(): void {
+    const date = new Date();
+    this.currentYear = date.getFullYear();
+  }
+
 }
